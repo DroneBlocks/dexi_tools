04708f649edbcb551b1bc7c4bce1741d88c22536
Dennis Baldwin <dennisbaldwin@gmail.com>
2025-09-18 18:59:23 -0500

    Fix executable installation setup
    - Move Python scripts from scripts/ to dexi_tools/ package directory
    - Update setup.py to use find_packages() and proper entry_points configuration
    - Add console_scripts for px_uploader and reset_fmu_wait_bl
    - Add flash_px4.sh as installable script
    - Follow same pattern as dexi_led package for consistency

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 import os
 from glob import glob
 
@@ -7,7 +7,7 @@ package_name = 'dexi_tools'
 setup(
     name=package_name,
     version='0.0.1',
-    packages=[package_name],
+    packages=find_packages(),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
@@ -25,6 +25,11 @@ setup(
     entry_points={
         'console_scripts': [
             'firmware_flash_node = dexi_tools.firmware_flash_node:main',
+            'px_uploader = dexi_tools.px_uploader:main',
+            'reset_fmu_wait_bl = dexi_tools.reset_fmu_wait_bl:main',
         ],
     },
+    scripts=[
+        'scripts/flash_px4.sh',
+    ],
 )
