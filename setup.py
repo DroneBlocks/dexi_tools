9679c68760ef03f435b12abd1334d94e3daaa799
Dennis Baldwin <dennisbaldwin@gmail.com>
2025-09-18 19:06:38 -0500

    Add setup.cfg and refine setup.py
    - Add setup.cfg to match dexi_led package structure
    - Update launch file glob pattern to be more robust
    - Remove redundant scripts data_files entry
    - Align with dexi_led package configuration

--- setup.py
+++ setup.py
@@ -12,8 +12,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
-        (os.path.join('share', package_name, 'scripts'), glob('scripts/*')),
+        # Include launch files
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
